2eef178292910c793e9cf0343d50b82a07b08aa9
Axyl07 <aryanchauhan000777@gmail.com>
2025-06-12 19:00:55 +0530

    Togglable button alignment fixed to switch bw editing and viewing mode

--- src/Components/Resume.jsx
+++ src/Components/Resume.jsx
@@ -11,16 +11,20 @@ export default function Resume({ name, email, number, schoolName, titleOfStudy, 
             <span>+91 {number} | {email}
             </span>
             </div>
-            ------------------------------------------------------------------------------------------------------------------------
-            <div>
+            ------------------------------------------------------------------------------------------------
+            <div style={{ display: 'flex', flexDirection:'column' }} >
                 <h2>Education:</h2>
-                <p>{schoolName}</p>
-                <span>{titleOfStudy} {dateOfStudy} </span>
+                <h3>School Name : {schoolName}</h3>
+                <span> Course : {titleOfStudy} <b>{dateOfStudy}(Graduation)</b> </span>
             </div>
-            ------------------------------------------------------------------------------------------------------------------------
+            ------------------------------------------------------------------------------------------------
             <div>
                 <h2>Work Experience</h2>
-                {companyName}{positionTitle}{responsibilities}{from}{to }
+                <p>Company Name : {companyName}</p>
+                <p>Title/Position : {positionTitle}</p>
+                <p>Responsibiliies : {responsibilities}</p>
+                <p>From : {from}</p>
+                <p>To : {to}</p>
             </div>
 
 
